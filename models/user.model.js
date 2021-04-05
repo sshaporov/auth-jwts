b69892f96d7977bf6172a15d2e433868a5072947
Sergey Shaporov <sshaporov86@gmail.com>
2021-04-05 23:09:51 +0300

    hash password

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema({
     email: {
@@ -12,6 +13,26 @@ const UserSchema = new Schema({
         type: String,
         required: true,
     },
+})
+
+UserSchema.pre('save', async function(next){
+    try{
+        // console.log('Called before saving a user')
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(this.password, salt)
+        this.password = hashedPassword
+        next()
+    } catch (err) {
+        next(err)
+    }
+})
+
+UserSchema.post('save', async function(next){
+    try{
+        // console.log('Called after saving a user')
+    } catch (err) {
+        next(err)
+    }
 })
 
 const User = mongoose.model('user', UserSchema)
