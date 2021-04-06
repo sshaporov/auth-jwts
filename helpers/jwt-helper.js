fb5d8595cd623121dfe246571f05f6bcb03d3640
Sergey Shaporov <sshaporov86@gmail.com>
2021-04-06 13:42:14 +0300

    create secret key for tokens using crypto generator

--- helpers/jwt-helper.js
+++ helpers/jwt-helper.js
@@ -5,7 +5,7 @@ module.exports = {
     signAccessToken: (userId) => {
         return new Promise((resolve, reject) => {
             const payload = {}
-            const secret = 'secret phrase'
+            const secret = process.env.ACCESS_TOKEN_SECRET
             const options = {
                 expiresIn: '1h',
                 issuer: 'sergey shaporov',
