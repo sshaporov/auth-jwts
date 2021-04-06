0c4ed9f0199afc334cc3b434918b4d2857f6173e
Sergey Shaporov <sshaporov86@gmail.com>
2021-04-06 22:52:15 +0300

    add logout logic and deleting refresh token

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,6 +4,7 @@ const createError = require('http-errors')
 const User = require('../models/user.model')
 const {authSchema} = require('../helpers/validation-schema')
 const {signAccessToken, signRefreshToken, verifyRefreshToken} = require('../helpers/jwt-helper')
+const client = require('../helpers/init-redis')
 
 router.post('/register', async (req, res, next) => {
     try {
@@ -65,11 +66,22 @@ router.post('/refresh-token', async (req, res, next) => {
 })
 
 router.delete('/logout', async (req, res, next) => {
-    res.send('logout route')
+    try {
+        const {refreshToken} = req.body
+        if(!refreshToken) throw createError.BadRequest()
+        const userId = await verifyRefreshToken(refreshToken)
+        client.DEL(userId, (err, value) => {
+            if(err) {
+                console.log(err.message)
+                throw createError.InternalServerError()
+            }
+            console.log(value)
+            res.sendStatus(204)
+        })
+    } catch (err) {
+        next(err)
+    }
 })
-
-
-
 
 
 module.exports = router
